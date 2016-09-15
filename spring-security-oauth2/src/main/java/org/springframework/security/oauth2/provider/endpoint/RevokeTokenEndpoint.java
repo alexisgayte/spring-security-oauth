6687edd7fc6b1d7d79de7017f42860a056d1c163
agyt <agyt@10.58.5.140>
2016-09-15 09:11:25 +0100

    fixed import

--- spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/endpoint/RevokeTokenEndpoint.java
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/endpoint/RevokeTokenEndpoint.java
@@ -33,6 +33,7 @@ import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
